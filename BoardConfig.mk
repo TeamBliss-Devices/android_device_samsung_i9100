4966b055327176ab502be8fa8abe70c5d698fd40
K Bell <krisnicbell@gmail.com>
2015-04-05 10:43:22 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,3 +33,4 @@ TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100
 
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i9100/BoardConfigVendor.mk
+-include vendor/bliss/config/sm.mk
